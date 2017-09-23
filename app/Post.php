05e2d1bbc3d15a0876e385ddc91705edba5e3edf
artis11 <artisk82@inbox.lv>
2017-09-23 22:35:17 +0300

    validate title as unique, use validation in model

--- app/Post.php
+++ app/Post.php
@@ -2,10 +2,27 @@
 
 namespace App;
 
+use Validator;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Validation\ValidationException;
 
 class Post extends Model
 {
+    private $rules = array(
+        'title' => 'required|unique:posts',
+        'text' => 'required|max:255',
+    );
+
+    public function validate($data)
+    {
+        $validator = Validator::make($data, $this->rules);
+
+        if ($validator->fails())
+        {
+            throw new ValidationException($validator);
+        }
+    }
+
     public function comments()
     {
         return $this->hasMany(Comment::class);
