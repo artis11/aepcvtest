0fe2923c7b892043a51959ebe69bd4d53479cc23
artis11 <artisk82@inbox.lv>
2017-09-24 10:23:00 +0300

    display creation and updated times for posts

--- resources/views/post/index.blade.php
+++ resources/views/post/index.blade.php
@@ -1,6 +1,10 @@
 <ul>
     @foreach ($posts as $post)
-        <li><a href="{{ action('PostController@show', $post) }}">{{ $post->id }}: {{ $post->title }}</a></li>
+        <li>
+                <a href="{{ action('PostController@show', $post) }}">{{ $post->id }}: {{ $post->title }}</a>
+                <p>Izveidošanas laiks: {{ Carbon\Carbon::parse($post->created_at)->format('Y/m/d H:i:s') }}</p>
+                <p>Pēdējo izmaiņu laiks: {{ Carbon\Carbon::parse($post->updated_at)->format('Y/m/d H:i:s') }}</p>
+        </li>
     @endforeach
 </ul>
 <p><a href="{{ action('PostController@create') }}">Pievienot jaunu ierakstu</a></p>
